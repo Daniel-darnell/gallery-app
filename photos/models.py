d690a44e792896271d9686f6c629bee9198b9b0a
Daniel-darnell <darnelldanny1997@gmail.com>
2020-11-21 04:39:00 -0500

    Added cloudinary to my application to be used in my model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Photo(models.Model):
+    image = CloudinaryField('image')
